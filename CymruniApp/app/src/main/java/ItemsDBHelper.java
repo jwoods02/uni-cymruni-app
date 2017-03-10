d291e246d25df1d147536d30516d06d614cc4b75
Kabira <sulemana@cardiff.ac.uk>
2017-03-10 16:47:45 +0000

    Created class ItemsDBHelper and added fields

---
+++ CymruniApp/app/src/main/java/ItemsDBHelper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by c1629177 on 10/03/2017.
+ */
+public class ItemsDBHelper {
+    public static final String TABLE_NAME = "ListItems";
+    public static final String COL_ID = "Item_ID";
+    public static final String COL_Item = "Item_Txt";
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Items.db";
+}
