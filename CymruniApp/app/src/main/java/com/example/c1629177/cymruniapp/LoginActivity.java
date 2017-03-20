07bedc20fe21563e6c97606f72ee18f7c341bf16
Daniel Dagley <dagleyd@cardiff.ac.uk>
2017-03-20 14:39:37 +0000

    Username and password added to login page

--- CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/LoginActivity.java
+++ CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/LoginActivity.java
@@ -5,13 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button submitBtn;
+    EditText usernameText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        submitBtn = (Button)findViewById(R.id.submitBtn);
+        usernameText = (EditText)findViewById(R.id.usernameText);
+        passwordText = (EditText)findViewById(R.id.passwordText);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (usernameText.getText().toString().equals("admin") && passwordText.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),"Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Incorrect Login Details",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         Button backBtn = (Button) findViewById(R.id.backBtn);
         backBtn.setOnClickListener(new View.OnClickListener() {
