1d2ae0cdf668c2e0d5740131560165516a7a37f3
Daniel Dagley <dagleyd@cardiff.ac.uk>
2017-03-20 15:09:50 +0000

    Localizing fixed

--- CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/MainActivity.java
+++ CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Dummy list, needs link to database
-        String[] welshSpeakingBusinesses = {"Hpffi Coffi", "Costa", "National Software Academy",
+        String[] welshSpeakingBusinesses = {"Hoffi Coffi", "Costa", "National Software Academy",
         "Applingua"};
 
 
