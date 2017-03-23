810e9d085512c43361eb33cfcb7cd364f26527ed
Kabira <sulemana@cardiff.ac.uk>
2017-03-23 11:34:24 +0000

    created ArrayList to categorize all Red Markers for fluent Welse

--- CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/MapsActivity.java
+++ CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/MapsActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,6 +16,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -48,13 +51,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // anonymous class syntax(same as lambda)
         btnMap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(mapReady)
-                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);            }
+                if (mapReady)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
         });
 
-        btnSatellite.setOnClickListener(new View.OnClickListener(){
-            public void onClick (View v){
-                if(mapReady)
+        btnSatellite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mapReady)
                     mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             }
         });
@@ -90,36 +94,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         CameraPosition target = CameraPosition.builder().target(studentUnionLatLng).zoom(14).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
 
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(51.488373, -3.177478))
-                        .title("Cardiff University Student Union")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                        .snippet("Fluent Welsh is spoken here"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.488396, -3.178465))
+                .title("Costa Coffee")
+                .snippet("Beginner level Welsh is spoken here")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
 
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(51.489635, -3.182219))
-                        .title("Hoffi Coffee")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.488259, -3.173882))
+                .title("Cafe 37")
+                .snippet("Intermediate Welsh is spoken here")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+
+        ArrayList RedMarker = new ArrayList();
+        RedMarker.add(mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.489635, -3.182219))
+                .title("Hoffi Coffee")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
 /* this is a custom marker - the ic_audiotrack needs to be changed!!!!!!! */
-                        .snippet("Fluent Welsh is spoken here"));
-
-
-
-        mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(51.488396, -3.178465))
-                        .title("Costa Coffee")
-                        .snippet("Beginner level Welsh is spoken here")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
-
-
-        mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(51.488259, -3.173882))
-                        .title("Cafe 37")
-                        .snippet("Intermediate Welse is spoken here")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
-
-
+                .snippet("Fluent Welsh is spoken here"))
+                .isVisible());
+        RedMarker.add(mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.488373, -3.177478))
+                .title("Cardiff University Student Union")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .snippet("Fluent Welsh is spoken here"))
+                .isVisible());
+        RedMarker.add(mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.488363, -3.182625))
+                .title("Welsh Government")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .snippet("Fluent Welsh is spoken here"))
+                .isVisible());
+        RedMarker.add(mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.485363, -3.178993))
+                .title("City Hall")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .snippet("Fluent Welsh is spoken here"))
+                .isVisible());
     }
 
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
 
+//        // Check which checkbox was clicked
+//        switch(view.getId()) {
+//            case R.id.Red:
+//                if (checked)
+//                RedMarker.get[i] //TODO copy all this into a text editor then git checkout then paste it but on a new branch and figure out how I can continue the checkobox activity
+//                else
+//                // Remove the meat
+//                break;
+//            case R.id.Orange:
+//                if (checked)
+//                // Cheese me
+//                else
+//                // I'm lactose intolerant
+//                break;
+//            case R.id.Yellow:
+//                if (checked)
+//                // Cheese me
+//                else
+//                // I'm lactose intolerant
+//                break;
+//            // TODO: Veggie sandwich
+    }
 }
