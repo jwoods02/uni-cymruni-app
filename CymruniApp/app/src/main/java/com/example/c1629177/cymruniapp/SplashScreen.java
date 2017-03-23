98e3adf19a3dcc87bc7b55fde5c03d725d9693b2
Kabira <sulemana@cardiff.ac.uk>
2017-03-23 14:28:48 +0000

    Created SplashScreen xml and timed thread start and finish

---
+++ CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.c1629177.cymruniapp;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent i = new Intent(getApplicationContext(),SplashScreen.class);
+                    startActivity(i);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
