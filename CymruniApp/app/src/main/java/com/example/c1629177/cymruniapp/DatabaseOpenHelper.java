c519fdb0c8bd424806f319ebbc9e280387af741f
Kabira <sulemana@cardiff.ac.uk>
2017-03-27 18:09:13 +0100

    Merge

--- CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/DatabaseOpenHelper.java
+++ CymruniApp/app/src/main/java/com/example/c1629177/cymruniapp/DatabaseOpenHelper.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "cymruNiDB.db";
+    private static String DATABASE_NAME = "cymruNiDB.db";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseOpenHelper(Context context) {
