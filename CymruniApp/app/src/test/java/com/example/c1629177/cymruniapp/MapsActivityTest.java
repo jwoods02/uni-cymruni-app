e3aa75bc0eb4417fa13814f4ebd9e77fb77ca0d8
Kabira <sulemana@cardiff.ac.uk>
2017-03-28 12:11:08 +0100

    started writing instrumation tests for Splashscreen and maps

---
+++ CymruniApp/app/src/test/java/com/example/c1629177/cymruniapp/MapsActivityTest.java
@@ -0,0 +1,23 @@
+package com.example.c1629177.cymruniapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by c1629177 on 28/03/2017.
+ */
+public class MapsActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+        
+
+    }
+
+    @Test
+    public void testOnCheckboxClicked() throws Exception {
+        assertArrayEquals();
+    }
+}
